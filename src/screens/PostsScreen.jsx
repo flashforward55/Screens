25edda5dacae9ee2d011c85132147634152411ef
Anton Styazhkin <rudana089@gmail.com>
2023-08-09 22:11:12 +0300

    Update PostsScreen.jsx

--- src/screens/PostsScreen.jsx
+++ src/screens/PostsScreen.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Image, Text, View, Pressable } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
 import { StyleSheet } from 'react-native';
 
 export const PostsScreen = () => {
@@ -8,10 +7,6 @@ export const PostsScreen = () => {
     <View style={{ paddingLeft: 16, paddingTop: 32 }}>
       <View style={styles.container}>
         <View style={styles.imageContainer}></View>
-        <View style={styles.imageAndPlusContainer}>
-          <View style={styles.imageContainer}></View>
-          <AntDesign name="pluscircleo" size={22} style={styles.imageAdd} color="#FF6C00" />
-        </View>
         <View style={{ marginTop: 'auto', marginBottom: 'auto' }}>
           <Text>Name</Text>
           <Text>Email</Text>
@@ -21,7 +16,6 @@ export const PostsScreen = () => {
   );
 };
 
-//export default PostsScreen;
 const styles = StyleSheet.create({
   container: {
     marginBottom: 32,
@@ -29,20 +23,10 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     gap: 8,
   },
-  imageAndPlusContainer: {
-    alignItems: 'center',
-    marginTop: 30,
-    maxWidth: '100%',
-    marginTop: -75,
-  },
   imageContainer: {
     backgroundColor: '#F6F6F6',
     width: 120,
     height: 120,
     borderRadius: 16,
-  },
-  imageAdd: {
-    top: -40,
-    left: 60,
   },
 });
