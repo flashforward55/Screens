e8bde8718331e71069010209e94f838670684a17
Anton Styazhkin <rudana089@gmail.com>
2023-08-10 21:38:27 +0300

    Add images

--- src/screens/PostsScreen.jsx
+++ src/screens/PostsScreen.jsx
@@ -1,42 +1,172 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { StyleSheet } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import * as SplashScreen from 'expo-splash-screen';
+import {
+  TouchableOpacity,
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  Dimensions,
+  FlatList,
+} from 'react-native';
+
+import { Feather, AntDesign } from '@expo/vector-icons';
+
+import { postsScreenArray } from '../data/posts';
+
+import UserAvatar from '../images/userAvatar.jpg';
 
 const PostsScreen = () => {
+  const navigation = useNavigation();
+
+  const [windowWidth, setWindowWidth] = useState(Dimensions.get('window').width);
+
+  const [posts, setPosts] = useState(postsScreenArray);
+
+  useEffect(() => {
+    const onChange = () => {
+      const width = Dimensions.get('window').width;
+      setWindowWidth(width);
+    };
+    const dimensionsHandler = Dimensions.addEventListener('change', onChange);
+
+    return () => dimensionsHandler.remove();
+  }, []);
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    await SplashScreen.hideAsync();
+  }, []);
+
   return (
-    <View
-      style={{
-        paddingLeft: 16,
-        paddingTop: 32,
-        flex: 1,
-        backgroundColor: '#FFFFFF',
-        borderTopWidth: 1,
-        borderTopColor: '#F6F6F6',
-      }}
-    >
-      <View style={styles.container}>
-        <View style={styles.imageContainer}></View>
-        <View style={{ marginTop: 'auto', marginBottom: 'auto' }}>
-          <Text>Name</Text>
-          <Text>Email</Text>
-        </View>
-      </View>
+    <View onLayout={onLayoutRootView} style={styles.container}>
+      <FlatList
+        ListHeaderComponent={
+          <View style={styles.userContainer}>
+            <Image style={styles.avatarImg} source={UserAvatar} />
+            <View style={styles.userInfo}>
+              <Text style={styles.userName}>Natali Romanova</Text>
+              <Text style={styles.userEmail}>email@example.com</Text>
+            </View>
+          </View>
+        }
+        data={posts}
+        renderItem={({ item }) => (
+          <View style={styles.itemList}>
+            <Image
+              source={item.img}
+              style={{
+                ...styles.cardImage,
+                width: windowWidth - 16 * 2,
+              }}
+            />
+            <Text style={styles.userPostTitle}>{item.title}</Text>
+            <View style={styles.userCard}>
+              <View style={styles.userCardInformation}>
+                <TouchableOpacity
+                  style={styles.wrap}
+                  onPress={() => navigation.navigate('Коментарі')}
+                >
+                  <Feather name="message-circle" size={24} color="#FF6C00" />
+                  <Text style={styles.textStatistic}>{item.comments}</Text>
+                </TouchableOpacity>
+                <View style={{ ...styles.wrap, marginLeft: 24 }}>
+                  <AntDesign name="like2" size={24} color="#FF6C00" />
+                  <Text style={styles.textStatistic}>{item.likes}</Text>
+                </View>
+              </View>
+              <View style={styles.wrap}>
+                <AntDesign name="enviromento" size={24} color="#BDBDBD" />
+                <Text style={styles.textStatistic}>{item.location}</Text>
+              </View>
+            </View>
+          </View>
+        )}
+        keyExtractor={item => item.id}
+      />
     </View>
   );
 };
+
 export default PostsScreen;
 
 const styles = StyleSheet.create({
   container: {
-    marginBottom: 32,
-    display: 'flex',
-    flexDirection: 'row',
-    gap: 8,
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
   },
-  imageContainer: {
-    backgroundColor: '#F6F6F6',
-    width: 120,
-    height: 120,
+  userContainer: {
+    marginVertical: 32,
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+  },
+  avatarImg: {
+    width: 60,
+    height: 60,
     borderRadius: 16,
+    resizeMode: 'cover',
+  },
+  userInfo: {
+    marginLeft: 8,
+    fontWeight: '700',
+  },
+  userName: {
+    color: '#212121',
+    fontSize: 13,
+    lineHeight: 15,
+    fontWeight: '400',
+  },
+  userEmail: {
+    color: '#212121',
+    opacity: 0.8,
+    fontSize: 11,
+    lineHeight: 13,
+  },
+  cardSection: {
+    alignItems: 'center',
+    width: '100%',
+    marginTop: 32,
+  },
+  itemList: {},
+  cardImage: {
+    resizeMode: 'cover',
+    borderRadius: 8,
+  },
+  userPostTitle: {
+    marginTop: 8,
+    fontSize: 16,
+    lineHeight: 19,
+    color: '#212121',
+    fontWeight: '500',
+  },
+  userCard: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 8,
+    marginBottom: 35,
+  },
+
+  userCardInformation: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  wrap: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textStatistic: {
+    marginLeft: 4,
+    fontSize: 16,
+    lineHeight: 19,
+    color: '#212121',
   },
 });
--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -1,76 +1,228 @@
-import React from 'react';
-import { ImageBackground, View, Pressable, Text, TouchableOpacity } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
-import image from '../images/bg.jpg';
-import { StyleSheet } from 'react-native';
-import { useEffect } from 'react';
-import { useIsFocused, useNavigation } from '@react-navigation/native';
+import React, { useState, useEffect, useCallback } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import {
+  TouchableOpacity,
+  StyleSheet,
+  View,
+  FlatList,
+  Image,
+  Text,
+  ImageBackground,
+  Dimensions,
+} from 'react-native';
+
+import { Feather, AntDesign } from '@expo/vector-icons';
+
+import { useNavigation } from '@react-navigation/native';
+
+import Bg from '../images/bg-image.png';
+import UserAvatarBig from '../images/userAvatarBig.jpg';
+
+import { profilePostArray } from '../data/posts';
 
 const ProfileScreen = () => {
-  const isFocused = useIsFocused();
   const navigation = useNavigation();
 
+  const [windowWidth, setWindowWidth] = useState(Dimensions.get('window').width);
+  const [windowHeight, setWindowHeight] = useState(Dimensions.get('window').height);
+
+  const [posts, setPosts] = useState(profilePostArray);
+
   useEffect(() => {
-    navigation.setOptions({
-      tabBarVisible: !isFocused,
-    });
-  }, [isFocused, navigation]);
+    const onChange = () => {
+      const width = Dimensions.get('window').width;
+      setWindowWidth(width);
+      const height = Dimensions.get('window').height;
+      setWindowHeight(height);
+    };
+    const dimensionsHandler = Dimensions.addEventListener('change', onChange);
+
+    return () => dimensionsHandler.remove();
+  }, []);
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    await SplashScreen.hideAsync();
+  }, []);
 
   return (
-    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-      <View style={[styles.container, { height: 549, paddingTop: 92 }]}>
-        <TouchableOpacity style={styles.imageAndPlusContainer}>
-          <View style={styles.imageContainer}></View>
-          <AntDesign name="pluscircleo" size={22} style={styles.imageAdd} color="#FF6C00" />
-        </TouchableOpacity>
-        <Pressable onPress={() => alert('You just loged out!')}>
-          <AntDesign
-            name="logout"
-            size={24}
-            color="black"
-            style={{ position: 'absolute', right: 16, top: -140 }}
-          />
-        </Pressable>
-        <Text style={styles.text}>Name</Text>
-      </View>
-    </ImageBackground>
+    <View onLayout={onLayoutRootView} style={styles.container}>
+      <ImageBackground style={styles.bgImage} source={Bg}>
+        <FlatList
+          ListHeaderComponent={
+            <View
+              style={{
+                ...styles.contentContainer,
+                marginTop: windowWidth > 500 ? 100 : 120,
+                width: windowWidth,
+              }}
+            >
+              <View
+                style={{
+                  ...styles.imageContainer,
+                  left: (windowWidth - 120) / 2,
+                }}
+              >
+                <Image style={styles.imageAvatar} source={UserAvatarBig} />
+              </View>
+              <View
+                style={{
+                  ...styles.userTitleContainer,
+                  width: windowWidth - 16 * 2,
+                }}
+              >
+                <Text style={styles.userTitle}>Natali Romanova</Text>
+              </View>
+            </View>
+          }
+          data={posts}
+          renderItem={({ item }) => (
+            <View
+              style={{
+                ...styles.postContainer,
+                width: windowWidth,
+              }}
+            >
+              <Image
+                source={item.img}
+                style={{
+                  ...styles.postImg,
+                  width: windowWidth - 16 * 2,
+                }}
+              />
+              <Text
+                style={{
+                  ...styles.postTitle,
+                  width: windowWidth - 30,
+                }}
+              >
+                {item.title}
+              </Text>
+              <View style={{ ...styles.statisticUser, width: windowWidth - 30 }}>
+                <View style={styles.row}>
+                  <TouchableOpacity
+                    style={styles.statisticWrap}
+                    onPress={() => navigation.navigate('Коментарі')}
+                  >
+                    <Feather name="message-circle" size={24} color="#FF6C00" />
+
+                    <Text style={styles.statisticText}>{item.comments}</Text>
+                  </TouchableOpacity>
+                  <View style={{ ...styles.statisticWrap, marginLeft: 24 }}>
+                    <AntDesign name="like2" size={24} color="#FF6C00" />
+
+                    <Text style={styles.statisticText}>{item.likes}</Text>
+                  </View>
+                </View>
+                <View style={styles.statisticWrap}>
+                  <AntDesign name="enviromento" size={24} color="#BDBDBD" />
+
+                  <Text style={styles.statisticText}>{item.location}</Text>
+                </View>
+              </View>
+            </View>
+          )}
+          keyExtractor={item => item.id}
+          contentContainerStyle={{
+            flexGrow: 1,
+            alignItems: 'center',
+
+            borderTopLeftRadius: 25,
+            borderTopRightRadius: 25,
+          }}
+          showsVerticalScrollIndicator={false}
+        />
+      </ImageBackground>
+    </View>
   );
 };
 
 export default ProfileScreen;
 
-export const styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
-    position: 'relative',
-    backgroundColor: '#fff',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+  },
+  bgImage: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  contentContainer: {
+    backgroundColor: '#FFFFFF',
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
-  },
-  image: {
-    flex: 1,
-    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
 
-  imageAndPlusContainer: {
-    alignItems: 'center',
-    marginTop: 30,
-    maxWidth: '100%',
-    marginTop: -75,
-  },
   imageContainer: {
-    backgroundColor: '#F6F6F6',
+    position: 'absolute',
+    top: -60,
     width: 120,
     height: 120,
+    backgroundColor: '#F6F6F6',
     borderRadius: 16,
   },
-  imageAdd: {
-    top: -40,
-    left: 60,
+  imageAvatar: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 16,
+    resizeMode: 'cover',
   },
-  text: {
+  userTitleContainer: {
+    alignItems: 'center',
+    marginTop: 90,
+    marginBottom: 30,
+  },
+  userTitle: {
+    textAlign: 'center',
     fontSize: 30,
-    marginBottom: 92,
-    marginLeft: 'auto',
-    marginRight: 'auto',
+    lineHeight: 35,
+    color: '#212121',
+    fontWeight: '500',
+  },
+  postContainer: {
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+  },
+  postImg: {
+    resizeMode: 'cover',
+    borderRadius: 8,
+  },
+  postTitle: {
+    marginTop: 8,
+    fontSize: 16,
+    lineHeight: 19,
+    color: '#212121',
+    fontWeight: '500',
+  },
+  statisticUser: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 8,
+    marginBottom: 35,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  statisticWrap: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  statisticText: {
+    marginLeft: 4,
+    fontSize: 16,
+    lineHeight: 19,
+    color: '#212121',
   },
 });
