d1e7333609f417bc39227b92b055e3bb6a5c5874
Anton Styazhkin <rudana089@gmail.com>
2023-10-16 10:35:02 +0300

    Update toasts.js

--- src/utils/toasts.js
+++ src/utils/toasts.js
@@ -1,105 +1,101 @@
-import Toast, {
-  BaseToast,
-  ErrorToast,
-  InfoToast,
-} from "react-native-toast-message";
+import Toast, { BaseToast, ErrorToast, InfoToast } from 'react-native-toast-message';
 
 export const successLoginToast = () =>
   Toast.show({
-    type: "success",
-    text1: "Ви увійшли",
-    text2: "Керуйте своєю колекцією фото",
+    type: 'success',
+    text1: 'Ви увійшли',
+    text2: 'Керуйте своєю колекцією фото',
   });
 
 export const successDeleteAvatarToast = () =>
   Toast.show({
-    type: "success",
-    text1: "Привітання!",
-    text2: "Ваш аватар видалено",
+    type: 'success',
+    text1: 'Привітання!',
+    text2: 'Ваш аватар видалено',
   });
 
 export const successAddPostToast = () =>
   Toast.show({
-    type: "info",
-    text1: "Ваш новий пост додано",
-    text2: "Поставте лайк і залиште коментар :)",
+    type: 'info',
+    text1: 'Ваш новий пост додано',
+    text2: 'Поставте лайк і залиште коментар :)',
   });
 
 export const errorRegistrationToast = () =>
   Toast.show({
-    type: "error",
-    text1: "Реєстрація неможлива!",
-    text2: "Цей імейл вже зареєстрований",
+    type: 'error',
+    text1: 'Реєстрація неможлива!',
+    text2: 'Цей імейл вже зареєстрований',
   });
 
 export const errorLoginToast = () =>
   Toast.show({
-    type: "error",
-    text1: "Електронна пошта або пароль не співпадають",
-    text2: "Перевірте їх і спробуйте ще раз",
+    type: 'error',
+    text1: 'Електронна пошта або пароль не співпадають',
+    text2: 'Перевірте їх і спробуйте ще раз',
   });
 
 export const errorFormToast = () =>
   Toast.show({
-    type: "error",
-    text1: "Помилка. Який сором!",
-    text2: "Аватар і всі поля не повинні бути порожніми",
+    type: 'error',
+    text1: 'Помилка. Який сором!',
+    text2: 'Аватар і поля повинні заповнені',
   });
 
 export const errorCommentsToast = () =>
   Toast.show({
-    type: "error",
-    text1: "Коментар не повинен бути порожнім",
-    text2: "Не менше 10 і <200 символів",
+    type: 'error',
+    text1: 'Коментар не повинен бути порожнім',
+    text2: 'Не менше 10 і <200 символів',
   });
 
 export const errorAcceptCameraToast = () =>
   Toast.show({
-    type: "error",
-    text1: "Ви не дали дозволу",
-    text2: "У доступі до місцезнаходження відмовлено",
+    type: 'error',
+    text1: 'Ви не дали дозволу',
+    text2: 'У доступі до місцезнаходження відмовлено',
   });
 
 export const toastConfig = {
-  success: (props) => (
+  success: props => (
     <BaseToast
       {...props}
-      style={{ borderLeftColor: "green" }}
+      style={{ borderLeftColor: 'green' }}
       text1Style={{
-        color: "green",
+        color: 'green',
         fontSize: 18,
       }}
       text2Style={{
         fontSize: 16,
-        color: "gray",
+        color: 'gray',
       }}
     />
   ),
-  error: (props) => (
+  error: props => (
     <ErrorToast
       {...props}
-      style={{ borderLeftColor: "red" }}
+      style={{ borderLeftColor: 'red' }}
       text1Style={{
-        color: "red",
+        color: 'red',
         fontSize: 18,
       }}
       text2Style={{
         fontSize: 16,
-        color: "gray",
+        color: 'gray',
       }}
     />
   ),
-  info: (props) => (
+  info: props => (
     <InfoToast
       {...props}
-      style={{ borderLeftColor: "blue" }}
+      style={{ borderLeftColor: 'blue' }}
       text1Style={{
-        color: "darkblue",
+        color: 'darkblue',
         fontSize: 18,
       }}
       text2Style={{
         fontSize: 16,
-        color: "gray",
+        color: 'gray',
       }}
     />
   ),
